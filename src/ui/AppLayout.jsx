aa482b7081728048e8531a9739cc74cb4a3a0202
Chiaraliz <15962100649@163.com>
2024-07-06 18:18:06 +0800

    Add menu data fetching, loader and error page

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,14 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
-function AppLayout() {
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
 
       <main>
@@ -14,5 +19,3 @@ function AppLayout() {
     </div>
   );
 }
-
-export default AppLayout;
